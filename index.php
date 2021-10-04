dce8a734cb7188672b4255255ddb7611922273e7
AleMARodrigues <91918799+AleMARodrigues@users.noreply.github.com>
2021-10-04 18:26:03 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Aprendendo básico de CSS</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="estilo.css" type="text/css">
+  </head>
+  <body class="tela-azul">
+    Alessandra e Matheus
+  </body>
+</html>
